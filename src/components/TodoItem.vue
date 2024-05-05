c0a5788e39372aa9aba65829b7f140ba263a815a
y <dennis.erdelean@protonmail.com>
2024-05-05 14:26:52 +0200

    final stylings

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,11 @@
 <template>
   <transition name="fade">
-    <div v-if="!isCompleted" class="mt-8 bg-gray-800 text-gray-200 border-gray-700 border-2 rounded-md shadow-lg hover:bg-gray-700 transition-colors duration-200">
+    <div v-if="!isCompleted" class="flex items-center justify-between mt-8 bg-gray-800 text-gray-200 border-gray-700 p-4 border-2 rounded-md shadow-lg hover:bg-gray-700 transition-colors duration-200 font-bold">
       <i 
-        :class="{ 'active': props.todo?.completed }" 
-        class="material-icons" @click="handleCompleteTodo(props.todo?.id)"
+        :class="['material-icons', 'cursor-pointer', 'active:text-gray-700', props.todo?.completed ? 'text-blue-500' : '']"  
+        @click="handleCompleteTodo(props.todo?.id)"
         :key="props.todo?.completed"
-        title="Archive"
+        :title="$props.todo?.completed ? 'Back to Active' : 'Complete'"
       >
         {{ props.todo?.completed ? 'check_box' : 'check_box_outline_blank' }}
       </i>
@@ -14,15 +14,17 @@
         v-model="editableTitle" 
         @blur="handleSubmit" 
         @keyup.enter="handleSubmit" 
+        class="form"
       />
       <h3 v-else :class="{ 'completed': props.todo?.completed }">
         {{ props.todo?.title }}</h3>
-      <div class="icons">
-        <span>
-          {{ props.todo?.date?.toLocaleDateString() }}
-        </span>
-        <select 
-        :class="['todo', priorityClass(props.todo?.priority)]"
+        <div class="flex flex-col items-end space-y-2">
+          <span class="text-xs">
+            {{ props.todo?.date?.toLocaleDateString() }}
+          </span>
+        <div class="flex space-x-4">
+          <select 
+        :class="['w-6 h-6 rounded-full','appearance-none', 'text-transparent', 'outline-none', priorityClass(props.todo?.priority)]"
         v-model="newPriority" 
         @change="handlePriorityChange($event.target as HTMLSelectElement)"
       >
@@ -31,19 +33,20 @@
         <option value="3">Low</option>
       </select>
       <i 
-        class="material-icons " 
+        class="material-icons cursor-pointer text-gray-500 hover:text-white active:text-gray-700 " 
         @click="editing = !editing"
         title="Edit"
       >
         edit
       </i>
       <i 
-        class="material-icons trash " 
+        class="material-icons trash cursor-pointer text-gray-500 hover:text-white active:text-gray-700 " 
         @click="handleRemoveTodo(props.todo?.id)"
         title="'Remove'"
       >
         delete
       </i>
+    </div>
       </div>
     </div>
   </transition>
@@ -104,11 +107,11 @@ const handleRemoveTodo = (id: number) => {
 const priorityClass = (priority: number) => {
   switch(priority) {
     case 1: 
-      return "high-priority";
+      return "bg-red-500";
     case 2: 
-      return "medium-priority";
+      return "bg-yellow-500";
     case 3: 
-      return "low-priority";
+      return "bg-green-500";
     default: 
       return "";
      }
